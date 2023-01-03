803c913f93fd5a96a1651837769ce5556c62ea92
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-03 09:53:05 +0000

    Refactor / create AboutUs section

---
+++ components/AboutUs/AboutUs.tsx
@@ -0,0 +1,67 @@
+import { SectionHeading } from 'components/SectionHeading';
+import { Section, Container, Grid, GridColumn } from 'components/Layout';
+import { Button } from 'components/Button';
+import { BulletPoints } from './BulletPoints';
+import { SanityBlockContent } from 'components/SanityBlockContent';
+
+import { SCREEN_SIZE_MD } from 'lib/constants';
+import { useMediaQuery } from 'lib/hooks/useMediaQuery'
+
+import styles from './AboutUs.module.scss';
+
+export const AboutUs = ({ ...rest }) => {
+
+  const { title, subtitle, description, link, bulletPoints } = rest;
+  const isMobile = useMediaQuery(`(max-width: ${SCREEN_SIZE_MD})`);
+
+  const CTA = () => {
+    if (!link?.page?.slug?.current) return null;
+    return (
+      <div className={styles.btnWrapper}>
+        <Button
+          variant={'secondary'}
+          link={link?.page}
+          onDark={false}
+        >
+          {link?.title}
+        </Button>
+      </div>
+    )
+  }
+  
+  return (
+    <Section
+      size={'md'}
+      bg={'light'}
+      color={'primary'}
+    >
+      <Container>
+        <Grid
+          justifyContent={'space-between'}
+          alignItems={'start'}
+          fullWidth={true}
+        >
+          <GridColumn sm={12} md={6} lg={6}>
+            <SectionHeading
+              title={title}
+              subtitle={subtitle}
+              headingType={'h2'}
+              align={'left'} />
+            {!isMobile && <CTA />}
+          </GridColumn>
+          <GridColumn sm={12} md={6} lg={6} className={styles.description}>
+            {Array.isArray(description) ? (
+              <SanityBlockContent text={description} />
+            ) : (
+              <p>{description}</p>
+            )}
+            {isMobile && <CTA />}
+          </GridColumn>
+        </Grid>
+        {bulletPoints?.length > 0 && <BulletPoints bulletPoints={bulletPoints} />}
+      </Container>
+    </Section>
+  );
+};
+
+export default AboutUs;
--- lib/shared-domain/page/presentation/contentModules/AboutUsSection.tsx
+++ lib/shared-domain/page/presentation/contentModules/AboutUsSection.tsx
@@ -1,78 +1,14 @@
-import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
-import { Box, FlexCol, FlexRow } from 'components/Layout/Flex/Flex';
-import { P } from 'components/Typography/P';
 import React from 'react';
 
 import { AboutUsSectionModule } from '../../domain/contentModule';
-import { LinkWithArrow } from '../components/LinkWithArrow';
-import { TitleWithSubtitleAndDescription } from '../components/TitleWithSubtitleAndDescription';
 
-import { icons } from '../../../../../components/Icons/index';
-import { H } from 'components/Typography/H';
-import { fontWeights } from '../../../../../styles/foundations/fontStyles';
-import { SectionContainer } from 'components/Layout/SectionContainer';
-import { SanityBlockContent } from 'components/SanityBlockContent';
-import { colors } from 'styles/foundations/colors';
+import AboutUs from 'components/AboutUs';
 
 export const AboutUsSection: React.FC<{
   specificContentModule: AboutUsSectionModule;
 }> = ({ specificContentModule }) => {
-  // const isMobile = useBreakpointValue({ base: true, lg: false });
-
   return (
-    <></>
-    // <FlexCol
-    //   backgroundImage={`url(${specificContentModule.getBackgroundImage()})`}
-    //   backgroundPosition="center"
-    //   backgroundSize="cover"
-    //   backgroundRepeat="no-repeat"
-    //   backgroundAttachment={!isMobile && 'fixed'}
-    // >
-    //   <SectionContainer py="md">
-    //     <Box width={{ base: '100%', lg: '50%' }}>
-    //       <TitleWithSubtitleAndDescription
-    //         {...specificContentModule}
-    //         description={[]}
-    //       />
-    //     </Box>
-    //     <Grid templateColumns="repeat(auto-fit, minmax(330px, 1fr))" gap={12}>
-    //       <GridItem>
-    //         <SanityBlockContent text={specificContentModule.description} />
-
-    //         <Box mt={4}>
-    //           <LinkWithArrow
-    //             href={specificContentModule?.link?.page?.slug?.current}
-    //             title={specificContentModule?.link?.title}
-    //           />
-    //         </Box>
-    //       </GridItem>
-    //       <GridItem>
-    //         <Box>
-    //           {specificContentModule?.bulletPoints?.map((b, index) => (
-    //             <Box mb={4} key={index}>
-    //               <H fontSize="24px" fontWeight={fontWeights.highlight} mb={1}>
-    //                 {b.title}
-    //               </H>
-    //               {b.texts.map((t, index) => (
-    //                 <FlexRow mb={2} key={index} alignItems="center">
-    //                   <Box
-    //                     transform="rotate(180deg)"
-    //                     style={{ marginBottom: '-4px' }}
-    //                   >
-    //                     <icons.ChevronLeft color="black" />
-    //                   </Box>
-    //                   <P color={colors.text.light} ml={2}>
-    //                     {t}
-    //                   </P>
-    //                 </FlexRow>
-    //               ))}
-    //             </Box>
-    //           ))}
-    //         </Box>
-    //       </GridItem>
-    //     </Grid>
-    //   </SectionContainer>
-    // </FlexCol>
+    <AboutUs {...specificContentModule} />
   );
 };
 
