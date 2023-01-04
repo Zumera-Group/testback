fe3dac327a8bd615cdd29444b965a145925e4ea9
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-04 18:06:15 +0000

    Feat: Refactor/create LeadershipTeam component

---
+++ components/LeadershipTeam/LeadershipTeam.tsx
@@ -0,0 +1,81 @@
+import { Section, Container, Grid, GridColumn } from 'components/Layout';
+import { SectionHeading } from 'components/SectionHeading';
+import { Employee } from 'components/NewsGrid';
+
+import { useFetchEmployees } from 'lib/shared-domain/employees/application/useGetEmployees';
+
+import styles from './LeadershipTeam.module.scss';
+
+interface Props {
+  title?: string;
+  subtitle?: string;
+  description?: any;
+}
+
+export const LeadershipTeam: React.FC<Props> = ({ ...rest }) => {
+  const { title, subtitle, description } = rest;
+
+  const fallbackIndex = 999;
+  const employees = useFetchEmployees();
+
+  const leadershipTeamMembers =
+    employees?.filter(
+      (e) =>
+        !e.hasLeftTheTeam &&
+        !!e.isInLeadershipTeam &&
+        e.leadershipListIndex !== 0,
+    ) || [];
+
+  const bigLeadershipTeamMembers =
+    employees?.filter(
+      (e) =>
+        !e.hasLeftTheTeam &&
+        !!e.isInLeadershipTeam &&
+        e.leadershipListIndex === 0,
+    ) || [];
+
+  const orderedLeadershipTeamMembers = leadershipTeamMembers.sort((a, b) =>
+    (a.leadershipListIndex != null ? a.leadershipListIndex : fallbackIndex) <
+    (b.leadershipListIndex != null ? b.leadershipListIndex : fallbackIndex)
+      ? -1
+      : 1,
+  );
+
+  return (
+    <Section
+      size={'md'}
+      bg={'light'}
+      color={'primary'}
+    >
+      <Container>
+        <SectionHeading
+          title={title}
+          subtitle={subtitle}
+          description={description}
+          align={'center'}
+        />
+        <Grid
+          fullWidth={true}
+          justifyContent={'start'}
+          alignItems={'stretch'}
+          className={styles.teamGrid}
+        >
+          {[...bigLeadershipTeamMembers, ...orderedLeadershipTeamMembers]?.map((member, index) => (
+            <GridColumn
+              key={`leaderShipTeamMember-${index}`}
+              xs={12}
+              sm={6}
+              md={6}
+              lg={4}
+              className={styles.teamColumn}
+            >
+              <Employee article={member} />
+            </GridColumn>
+          ))}
+        </Grid>
+      </Container>
+    </Section>
+  );
+};
+
+export default LeadershipTeam;
--- lib/shared-domain/page/presentation/contentModules/LeadershipTeamSection.tsx
+++ lib/shared-domain/page/presentation/contentModules/LeadershipTeamSection.tsx
@@ -1,77 +1,14 @@
-import { Box } from 'components/Layout/Flex/Flex';
 import React from 'react';
-import useBreakpointValue from 'lib/shared-domain/useBreakpoint';
+
 import { LeadershipTeamSectionModule } from '../../domain/contentModule';
-import { TitleWithSubtitleAndDescription } from '../components/TitleWithSubtitleAndDescription';
 
-import { SectionContainer } from 'components/Layout/SectionContainer';
-import { Employee } from '../../../employees/domain/index';
-import { Grid, GridItem } from '@chakra-ui/react';
-import { EmployeeCard } from 'lib/shared-domain/employees/presentation/EmployeeCard';
-import { useFetchEmployees } from 'lib/shared-domain/employees/application/useGetEmployees';
-
-const fallbackIndex = 999;
+import LeadershipTeam from 'components/LeadershipTeam';
 
 export const LeadershipTeamSection: React.FC<{
   specificContentModule: LeadershipTeamSectionModule;
 }> = ({ specificContentModule }) => {
-  const employees = useFetchEmployees();
-  // const isMobile = useBreakpointValue({ base: true, md: false });
-  const isMobile = false;
-  const leadershipTeamMembers =
-    employees?.filter(
-      (e) =>
-        !e.hasLeftTheTeam &&
-        !!e.isInLeadershipTeam &&
-        e.leadershipListIndex !== 0,
-    ) || [];
-
-  const bigLeadershipTeamMembers =
-    employees?.filter(
-      (e) =>
-        !e.hasLeftTheTeam &&
-        !!e.isInLeadershipTeam &&
-        e.leadershipListIndex === 0,
-    ) || [];
-
-  const orderedLeadershipTeamMembers = leadershipTeamMembers.sort((a, b) =>
-    (a.leadershipListIndex != null ? a.leadershipListIndex : fallbackIndex) <
-    (b.leadershipListIndex != null ? b.leadershipListIndex : fallbackIndex)
-      ? -1
-      : 1,
-  );
-
   return (
-    <SectionContainer py="md">
-      <Box mb={4} width={{ base: '100%', lg: '50%' }}>
-        <TitleWithSubtitleAndDescription {...specificContentModule} />
-      </Box>
-      <Grid
-        templateColumns={{
-          base: '1fr',
-          lg: '1fr 1fr 1fr 1fr 1fr 1fr',
-        }}
-        gap={3}
-      >
-        {[...bigLeadershipTeamMembers, ...orderedLeadershipTeamMembers]?.map(
-          (p, index) => {
-            const getColspan = () => {
-              if (isMobile) return 1;
-              if (index === 0) return 4;
-              return 2;
-            };
-            return (
-              <GridItem colSpan={getColspan()} key={index}>
-                <EmployeeCard
-                  linkText={specificContentModule.linkText}
-                  employee={p}
-                />
-              </GridItem>
-            );
-          },
-        )}
-      </Grid>
-    </SectionContainer>
+    <LeadershipTeam {...specificContentModule} />
   );
 };
 
