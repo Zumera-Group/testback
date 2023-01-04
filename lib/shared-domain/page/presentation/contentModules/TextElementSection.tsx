cc0c9f544c8dd5491d661cc0523e3514bb7dd597
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-04 18:06:34 +0000

    Feat: Refactor/create TextElement component

--- lib/shared-domain/page/presentation/contentModules/TextElementSection.tsx
+++ lib/shared-domain/page/presentation/contentModules/TextElementSection.tsx
@@ -1,46 +1,14 @@
-import { Box, SimpleGrid } from '@chakra-ui/react';
-import useBreakpointValue from 'lib/shared-domain/useBreakpoint';
-import { P } from 'components/Typography/P';
 import React from 'react';
-import { SectionContainer } from '../../../../../components/Layout/SectionContainer';
+
 import { TextElementSectionModule } from '../../domain/contentModule';
-import { colors } from '../../../../../styles/foundations/colors';
-import { fontSizes } from '../../../../../styles/foundations/fontStyles';
+
+import TextElement from 'components/TextElement';
 
 export const TextElementSection: React.FC<{
   specificContentModule: TextElementSectionModule;
 }> = ({ specificContentModule }) => {
-  const firstBlock = specificContentModule.firstBlock[0].children[0].text;
-  const secondBlock = specificContentModule.secondBlock[0].children[0].text;
-  // const isMobile = useBreakpointValue({ base: true, lg: false });
-  const isMobile = false;
-  const TEXT_VARIANT = isMobile
-    ? 'mobileTextElementSectionFirstBlockP'
-    : 'textElementSectionFirstBlockP';
-
   return (
-    <SectionContainer py="md">
-      <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={{ base: 4, lg: 8 }}>
-        <Box>
-          <P
-            fontSize={fontSizes.h2}
-            variant={TEXT_VARIANT}
-            color={colors.text.light}
-          >
-            {firstBlock}
-          </P>
-        </Box>
-        <Box maxW={isMobile ? '100%' : '500px'}>
-          <P
-            fontSize={fontSizes.h2}
-            variant={TEXT_VARIANT}
-            color={colors.text.light}
-          >
-            {secondBlock}
-          </P>
-        </Box>
-      </SimpleGrid>
-    </SectionContainer>
+    <TextElement {...specificContentModule} />
   );
 };
 
