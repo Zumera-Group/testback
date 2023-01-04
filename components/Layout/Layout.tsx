7bdada32a38267babc8a72419a08701862cb5760
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-04 18:03:11 +0000

    Temp: remove unused component/page code

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,18 +1,18 @@
-import Footer from '../Footer';
-import Header from '../Header';
+// import Footer from '../Footer';
+// import Header from '../Header';
 
-interface Props {
-  children: React.ReactNode
-}
+// interface Props {
+//   children: React.ReactNode
+// }
 
-const Layout: React.FC<Props> = ({ children }) => {
-  return (
-    <main>
-      <Header />
-      {children}
-      <Footer />
-    </main>
-  );
-};
+// const Layout: React.FC<Props> = ({ children }) => {
+//   return (
+//     <main>
+//       <Header />
+//       {children}
+//       <Footer />
+//     </main>
+//   );
+// };
 
-export default Layout;
+// export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
 
-export default function Home() {
-  return (
-    <Layout>
-      <p>this is the layou</p>
-    </Layout>
-  );
-}
+// export default function Home() {
+//   return (
+//     <Layout>
+//       <p>this is the layou</p>
+//     </Layout>
+//   );
+// }
