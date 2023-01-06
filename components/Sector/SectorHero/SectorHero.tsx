7572b9d647edfcb4effee199cf53ec4a9affbbc7
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-06 17:25:34 +0000

    Feat: Refactor/create SectorHero on Sector page

---
+++ components/Sector/SectorHero/SectorHero.tsx
@@ -0,0 +1,92 @@
+import Image from 'next/image';
+import { Section, Container, Grid, GridColumn } from 'components/Layout';
+
+import { getEmployeeFullName } from 'lib/shared-domain/employees/domain/getEmployeeFullName';
+import { useFormatDate } from 'lib/shared-domain/useFormatDate';
+
+import baseStyles from 'components/Hero/Hero.module.scss'
+import styles from './SectorHero.module.scss';
+
+export const SectorHero = ({ sector, content }) => {
+  const format = useFormatDate();
+
+  const contributorsAsText = sector?.contributors
+    ?.map((c) => getEmployeeFullName(c))
+    .join(', ');
+
+  const t = (item) => content?.[item];
+
+  const Detail = ({ as, title, text }) => {
+    const Component = as || 'div'
+    return (
+      <Component className={styles.detail}>
+        <h3 className={styles.detail_title}>{title}</h3>
+        <p className={styles.detail_text}>{text}</p>
+      </Component>
+    );
+  };
+
+  return (
+    <Section
+      as={'div'}
+      classes={[baseStyles.hero].join(' ')}
+      size={'lg'}
+      bg={'light'}
+      color={'primary'}
+    >
+      <Container>
+        <Grid
+          fullWidth={true}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+        >
+          <GridColumn sm={12} md={7} lg={7}>
+            <h1 className={styles.title}>
+              {sector.name && sector.name}
+            </h1>
+            {sector.description && <p>{sector.description}</p>}
+          </GridColumn>
+          <GridColumn sm={12} md={5} lg={5}>
+            {sector.detailPageHeroImage?.asset?.url && (
+              <div className={styles.imageWrapper}>
+                <Image
+                  unoptimized
+                  src={sector.detailPageHeroImage?.asset?.url}
+                  alt={'sector.name'}
+                  layout={'fill'}
+                  objectFit={'contain'}
+                  objectPosition={'center center'}
+                />
+              </div>
+            )}
+          </GridColumn>
+        </Grid>
+        <ul className={styles.details}>
+          {sector.date && (
+            <Detail
+              as={'li'}
+              title={t('lateUpdate')}
+              text={format(new Date(sector.date))}
+            />
+          )}
+          {sector.type && (
+            <Detail
+              as={'li'}
+              title={t('type')}
+              text={sector.type}
+            />
+          )}
+          {contributorsAsText && (
+            <Detail
+              as={'li'}
+              title={t('contributors')}
+              text={contributorsAsText}
+            />
+          )}
+        </ul>
+      </Container>
+    </Section>
+  );
+};
+
+export default SectorHero;
