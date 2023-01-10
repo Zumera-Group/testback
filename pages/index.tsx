0ef0cab0f27474f3c04dc33f64df511d403f3b0d
Tom Small <109525303+tomsmallON@users.noreply.github.com>
2023-01-10 19:27:51 +0000

    Fix: re-instate page with React component as default export

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 // import Layout from '../components/Layout'
 
-// export default function Home() {
-//   return (
-//     <Layout>
-//       <p>this is the layou</p>
-//     </Layout>
-//   );
-// }
+export default function Home() {
+  return (
+    <div>Index</div>
+    // <Layout>
+    //   <p>this is the layou</p>
+    // </Layout>
+  );
+}
