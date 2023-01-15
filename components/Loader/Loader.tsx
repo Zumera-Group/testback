47af2277d74541ab8d0f701dffb7ce8231ca2eaf
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2023-01-15 19:37:06 +0200

    Fix build issue

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 import styles from './Loader.module.scss';
 
-export const Loader: React.FC<{ className: string }> = ({ className }) => {
+export const Loader: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div className={`${styles.loader} ${className}`}>
       <span></span>
