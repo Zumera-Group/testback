e95527bbc69a067af7304c67493736293438c5b7
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2023-01-17 12:49:13 +0200

    1. Create service tabs component 2. Create Service TransactionShowcase section

---
+++ components/Service/ServiceTabs/ServiceTabs.tsx
@@ -0,0 +1,38 @@
+import { Section, Container, Grid } from 'components/Layout';
+import { Tab } from 'lib/shared-domain/page/domain';
+import { Button } from 'components/Button';
+interface ServiceTabsProps {
+  tabs: Tab[];
+  onSelectTab(t: Tab): void;
+  activeTabKey: string;
+}
+
+export const ServiceTabs: React.FC<any> = ({
+  tabs,
+  activeTabKey,
+  onSelectTab,
+}) => {
+  console.log(activeTabKey);
+  return (
+    <Section size={'md'} bg={'light'} color={'primary'}>
+      <Container>
+        <Grid
+          fullWidth={true}
+          justifyContent={'space-around'}
+          alignItems={'center'}
+        >
+          {tabs.map((tab) => (
+            <Button
+              hideIcon={true}
+              key={tab._key}
+              variant={activeTabKey !== tab._key ? 'secondary' : 'primary'}
+              callBack={() => onSelectTab(tab)}
+            >
+              {tab.title}
+            </Button>
+          ))}
+        </Grid>
+      </Container>
+    </Section>
+  );
+};
