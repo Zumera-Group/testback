f3886d76905819dcbbdb4ba6d67e2ae963e43860
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2023-01-26 21:27:15 +0200

    Create TransactionInvolvedParties (Quotes)

---
+++ components/Transaction/TransactionInvolvedParties/index.tsx
@@ -0,0 +1,42 @@
+import { Container, Grid, Section } from 'components/Layout';
+import { optionalUI } from 'lib/shared-domain/transactions/domain';
+import Image from 'next/image';
+import styles from './TransactionInvolvedParties.module.scss';
+export const TransactionInvolvedParties: React.FC<{
+  optionalUI: optionalUI;
+}> = ({ optionalUI }) => {
+  const { involvedParty1, involvedParty2 } = optionalUI;
+  const parties = [involvedParty1, involvedParty2];
+  return (
+    <Section size={'md'} bg={'light'} color={'primary'}>
+      <Container classes={styles.container}>
+        <Grid
+          fullWidth={true}
+          justifyContent={'center'}
+          alignItems={'center'}
+          className={styles.sliderHeader}
+        >
+          {parties.map((party) => (
+            <div key={party.name} className={styles.party}>
+              <div className={styles.image}>
+                <Image
+                  unoptimized
+                  loading="lazy"
+                  src={party.companyLogo?.asset?.url}
+                  alt={party.name}
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <h4 className={styles.quote}>“{party.quote}”</h4>
+              <div>
+                <h4 className={styles.name}>{party.name}</h4>
+                <p>{party.jobTitle}</p>
+              </div>
+            </div>
+          ))}
+        </Grid>
+      </Container>
+    </Section>
+  );
+};
--- components/Transaction/index.ts
+++ components/Transaction/index.ts
@@ -3,3 +3,4 @@ export * from './TransactionCompany';
 export * from './TransactionTeam';
 export * from './TransactionSector';
 export * from './TransactionsSelected';
+export * from './TransactionInvolvedParties';
