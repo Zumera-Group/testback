52542fcf795be6d2dc7e6b649e909f36beb641b8
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2023-01-27 19:01:40 +0200

    Transaction page fixes

--- components/TransactionGridSection/TransactionGridSection.tsx
+++ components/TransactionGridSection/TransactionGridSection.tsx
@@ -21,6 +21,7 @@ import { Locale } from 'lib/locale';
 import { Button } from 'components/Button';
 
 interface IProps {
+  buttonText: string;
   dropdownsTitle: string;
   sectorsDropdown: ISectorsDropdown[];
 }
@@ -35,7 +36,7 @@ export const TransactionGridSection: React.FC<IProps> = (props) => {
   const [loading, setLoading] = useState(true);
   const [activeChunk, setActiveChunk] = useState(1);
   const [displayTransaction, setDisplayTransaction] = useState([]);
-
+  console.log(props);
   useEffect(() => {
     const transactionsToDisplay = transactions
       .filter((item) => {
@@ -173,7 +174,7 @@ export const TransactionGridSection: React.FC<IProps> = (props) => {
               callBack={() => setActiveChunk(activeChunk + 1)}
               classes={styles.loadMore}
             >
-              Load more +
+              {props.buttonText} +
             </Button>
           </Grid>
         ) : (
