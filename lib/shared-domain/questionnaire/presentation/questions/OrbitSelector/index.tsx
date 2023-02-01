4efba6719d52de9c140649dcdb34d91be7b8a7ba
MichaelaSB-ON <michaela@madebyon.com>
2023-02-01 19:48:26 +0000

    add updated range slider & output

--- lib/shared-domain/questionnaire/presentation/questions/OrbitSelector/index.tsx
+++ lib/shared-domain/questionnaire/presentation/questions/OrbitSelector/index.tsx
@@ -1,18 +1,10 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { Box, Flex, Input, useBreakpointValue } from '@chakra-ui/react';
-import { colors } from 'styles/foundations/colors';
-import { P } from 'components/Typography/P';
 import { Question } from 'lib/shared-domain/questionnaire/domain';
 import { useAnswers } from 'lib/shared-domain/questionnaire/application/useAnswers';
 import { QuestionButtons } from '../../Question/QuestionButtons';
 import { QuestionText } from '../../Question/QuestionText';
 import { QuestionAnimation } from '../../Question/QuestionAnimation';
 import { RequiredQuestionInfo } from '../../Question/RequiredQuestionInfo';
-import getAnswerFromDegree from './utils/getAnswerFromDegree';
-import getDegreeFromAnswer from './utils/getDegreeFromAnswer';
-import getCoordsPositionDiv from './utils/getCoordsPositionDiv';
-import getDegreeFromCoords from './utils/getDegreeFromCoords';
-import ArrowSelector from './ArrowSelector';
 import BackButton from 'components/Calculator/BackButton/BackButton';
 
 import styles from './Orbit.module.scss';
@@ -30,163 +22,80 @@ export const OrbitSelector: React.FC<{
   onPrevQuestion: () => void;
   currentPos: number;
 }> = ({ question, onNextQuestion, onPrevQuestion, currentPos }) => {
-  const isMobile = useBreakpointValue({ base: true, lg: false });
-  const [orbitWidthInPx, setOrbitWidthInPx] = useState(0);
-  const isMobileBtn = useMediaQuery(`(max-width: ${SCREEN_SIZE_MD})`);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const setOrbitHeightInPx = () => {
-    const totalMargin = 100;
-    const screenWidth = document?.body?.clientWidth;
-    const screenHeight = document?.body?.clientHeight;
-
-    const orbitWidthInPx =
-      screenWidth - totalMargin > 600 ? 600 : screenWidth - totalMargin;
-
-    if (isMobile) {
-      if (screenHeight > screenWidth) setOrbitWidthInPx(orbitWidthInPx);
-      else setOrbitWidthInPx(screenHeight);
-    } else {
-      setOrbitWidthInPx(600);
-    }
-  };
-  useLayoutEffect(() => {
-    setOrbitHeightInPx();
-  }, [setOrbitHeightInPx]);
-
   const { getAnswer, setAnswer } = useAnswers(question);
-  const sliderDom = useRef(undefined);
-  const doomElementToGetCoords = useRef(undefined);
-  const [sliderValue, setSliderValue] = useState(undefined);
-  const [canSliderMove, setCanSliderMove] = useState(undefined);
-  const [answer, setStringAnswer] = useState(getAnswer());
+  const isMobile = useMediaQuery(`(max-width: ${SCREEN_SIZE_MD})`);
+  const [value, setValue] = useState(10);
+  const rangeInput = useRef(null);
+  const MAX_VAL = 100;
 
-  const initSlide = () => {
-    setCanSliderMove(true);
-  };
-  const stopSlide = () => {
-    setCanSliderMove(false);
-  };
-  const updateSlide = (evt) => {
-    if (!canSliderMove) return;
-    const coords = getCoordsPositionDiv(evt, doomElementToGetCoords);
-
-    const mainCoord = {
-      x: orbitHeightInPx,
-      y: orbitHeightInPx,
-    };
-
-    const deg = getDegreeFromCoords(coords, mainCoord);
-    if (deg >= 0 && deg <= 180) {
-      setSliderValue(deg);
-      const answer = getAnswerFromDegree(
-        deg,
-        question?.answerSelector?.orbitSelector?.answerOptions,
-      );
-      setStringAnswer(answer);
-      setAnswer(answer);
-    }
+  const updateRange = () => {
+    const value = rangeInput.current.value;
+    rangeInput.current.style.background = `linear-gradient(to right, #f0005c 0%, #f0005c ${value}%, #300032 ${value}%, #300032 100%)`;
   };
 
   useEffect(() => {
-    if (answer) {
-      setSliderValue(
-        getDegreeFromAnswer(
-          answer,
-          question?.answerSelector?.orbitSelector?.answerOptions,
-        ),
-      );
-    }
-    setOrbitHeightInPx();
+    updateRange();
   }, []);
 
-  const orbitHeightInPx = orbitWidthInPx / 2;
-  const bgSize = orbitHeightInPx + 50;
+  //all answers options from sanity
+  const answers = question.answerSelector.orbitSelector.answerOptions;
+
+  //length of answer array
+  const answerLength =
+    question.answerSelector.orbitSelector.answerOptions.length;
+
+  //how much chart will be divided into
+  const segments = (answerLength / MAX_VAL) * 100;
+
+  //the value of each label e.g. 25% each in this case
+  const labelVal = MAX_VAL / segments;
+
+  // console.log(100 / segments);
+  // console.log(question.answerSelector.orbitSelector.answerOptions);
+
+  //for each answer get index and times by the divided segment value
+  //so 1 x 25, 2 x 25 etc..
+  // if the label value is greater than the value from the range but less than the value from other labels, then show
+  answers.forEach((el) => {
+    const index = answers.indexOf(el) + 1;
+
+    if (labelVal * index > value) {
+      console.log(index);
+    }
+  });
 
   return (
     <QuestionAnimation>
-      {isMobileBtn && (
+      {isMobile && (
         <BackButton onPrevQuestion={onPrevQuestion} currentPos={currentPos} />
       )}
       <QuestionText title={question.questionText}>
         <RequiredQuestionInfo isRequired={question?.isRequired} />
       </QuestionText>
 
-      <Box mt={90}>
-        <Box
-          bg={colors.orbitSelector}
-          bgSize="cover"
-          ref={doomElementToGetCoords}
-          margin="auto"
-          width={orbitWidthInPx + 'px'}
-          height={orbitHeightInPx + 'px'}
-          borderTopLeftRadius="100em"
-          borderTopRightRadius="100em"
-          borderBottom={0}
-          onTouchStart={() => initSlide()}
-          onMouseDown={(evt) => {
-            initSlide();
-            updateSlide(evt);
-          }}
-          onTouchMove={(evt) => updateSlide(evt)}
-          onMouseMove={(evt) => updateSlide(evt)}
-          onMouseUp={() => stopSlide()}
-          onMouseLeave={() => stopSlide()}
-          onTouchEnd={(evt) => {
-            initSlide();
-            updateSlide(evt);
-            stopSlide();
-          }}
-          onClick={(evt) => {
-            initSlide();
-            updateSlide(evt);
-            stopSlide();
-          }}
-          cursor={'pointer'}
-        >
-          <ArrowSelector
-            orbitHeightInPx={orbitHeightInPx}
-            sliderValue={sliderValue}
-          />
-
-          <Box
-            borderTopLeftRadius="100em"
-            borderTopRightRadius="100em"
-            bg="#300032"
-            width={bgSize + 'px'}
-            height={bgSize / 2 + 'px'}
-            margin="auto"
-            marginTop={orbitHeightInPx - bgSize / 2 + 'px'}
-          ></Box>
-        </Box>
-
-        <Box>
-          <Input
-            display="none"
-            ref={sliderDom}
-            type="range"
-            // width="100%"
-            min={0}
-            max={180}
-            value={sliderValue}
-            onChange={({ target: { value } }) => {
-              setSliderValue(value);
-            }}
-          ></Input>
-        </Box>
-      </Box>
-
-      <Flex mt={1} minH={4} justify="center">
-        <P variant="orbitSelector" color={'white'}>
-          {
-            question?.answerSelector?.orbitSelector?.answerOptions?.find(
-              (a) => a?.value === answer,
-            )?.label
+      <div className={styles.rangeWrapper}>
+        <output
+          className={styles.donut}
+          style={
+            {
+              '--percentage': value,
+            } as React.CSSProperties
           }
-        </P>
-      </Flex>
+        />
+        <input
+          ref={rangeInput}
+          className={styles.range}
+          type="range"
+          min={0}
+          max={MAX_VAL}
+          value={value}
+          onInput={updateRange}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
+      </div>
 
       <div className={styles.buttonOuter}>
-        {!isMobileBtn && (
+        {!isMobile && (
           <BackButton onPrevQuestion={onPrevQuestion} currentPos={currentPos} />
         )}
 
