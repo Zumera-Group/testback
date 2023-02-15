8a8a3250871bb30e61c96bb746eccfd8bc9f27e9
MichaelaSB-ON <michaela@madebyon.com>
2023-02-15 00:18:32 +0000

    remove chakra reference from calc teaser comp

--- lib/shared-domain/questionnaire/presentation/CalculatorTeaser.tsx
+++ lib/shared-domain/questionnaire/presentation/CalculatorTeaser.tsx
@@ -1,44 +1,10 @@
-import result_teaserReact from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useBreakpointValue, VStack, Flex, Hide, Show } from '@chakra-ui/react';
-import { Btn } from 'components/Buttons/Button';
-import { Box, FlexRow } from 'components/Layout/Flex/Flex';
-import { SectionContainer } from 'components/Layout/SectionContainer';
-import { P } from 'components/Typography/P';
 import { TitleWithSubtitleAndDescription } from 'lib/shared-domain/page/presentation/components/TitleWithSubtitleAndDescription';
 import { colors } from 'styles/foundations/colors';
 import { TextBoxGroup } from './TextBoxGroup';
-
-import resultTeaser from '../../../../public/contentModules/calculatorTeaserSection/result_teaser.svg';
-import { FlexCol } from '../../../../components/Layout/Flex/Flex';
 import { useRouter } from 'next/router';
-import { Beam } from 'components/Beam';
 import styles from './CalculatorTeaser.module.scss';
 import { HalfBeam } from 'components/HalfBeam';
 import { Button } from 'components/Button';
-
-const LineSpacerMobile = () => {
-  return (
-    <Box
-      position="relative"
-      h={1}
-      w={6}
-      borderBottom={`1px solid ${colors.text.light}`}
-    ></Box>
-  );
-};
-
-// const TeaserBoxDesktop: React.FC<{}> = ({ children }) => {
-//   const BOX_SPACE_HEIGHT = '25%';
-//   return (
-//     <FlexRow h={BOX_SPACE_HEIGHT}>
-//       <Flex align="center" justify="center">
-//         {children}
-//       </Flex>
-//     </FlexRow>
-//   );
-// };
 
 export const ResultTeaser: React.FC<{
   calculatorSteps: {
@@ -49,9 +15,6 @@ export const ResultTeaser: React.FC<{
   };
   isSectorSpecificEntry?: boolean;
 }> = ({ calculatorSteps, isSectorSpecificEntry }) => {
-  const isMobile = useBreakpointValue({ base: true, lg: false });
-  const isMobileOrisSectorSpecificEntry = isMobile || isSectorSpecificEntry;
-
   return (
     <div className={styles.calculatorTeaserWrapper}>
       <HalfBeam />
@@ -100,9 +63,6 @@ export const CalculatorTeaser: React.FC<{
   buttonText,
   calculatorSteps,
 }) => {
-  const CIRCLE_BORDER = `1px solid ${colors.primary.darkGreen}`;
-  const MARGIN_TOP = 6.5;
-  const isMobile = useBreakpointValue({ base: true, lg: false });
   const router = useRouter();
   const page = router.locale === 'en' ? 'questionnaires' : 'fragenkatalog';
   if (!questionnaireSlug) return null;
