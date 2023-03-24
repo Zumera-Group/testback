df615b6fc5cd7627016bba9ff8498ddc692daced
MichaelaSB-ON <michaela@madebyon.com>
2023-03-24 12:04:03 +0000

    tweak anchor positioning

--- components/OpenJobsList/index.tsx
+++ components/OpenJobsList/index.tsx
@@ -3,6 +3,7 @@ import { SectionHeading } from 'components/SectionHeading';
 import { useFetchJobs } from 'lib/shared-domain/jobs/application/useGetJobs';
 import { JobItem } from 'components/OpenJobsList/JobItem';
 import { useRouter } from 'next/router';
+import styles from './OpenJobsList.module.scss';
 
 export const OpenJobsList: React.FC<any> = ({ specificContentModule }) => {
   const { title, subtitle, description } = specificContentModule;
@@ -14,33 +15,36 @@ export const OpenJobsList: React.FC<any> = ({ specificContentModule }) => {
   );
   console.log(sortedJobs);
   return (
-    <Section size={'md'} bg={'light'} color={'primary'} id={'jobsList'}>
-      <Container>
-        <Grid
-          justifyContent={'space-between'}
-          alignItems={'start'}
-          fullWidth={true}
-        >
-          <GridColumn sm={12} md={7} lg={7}>
-            <SectionHeading
-              title={title}
-              subtitle={subtitle}
-              description={description}
-              headingType={'h2'}
-              align={'left'}
-            />
-          </GridColumn>
-          <GridColumn sm={12} md={12} lg={12}>
-            {sortedJobs.map((item) => (
-              <JobItem
-                key={item._id}
-                job={item}
-                personioTrackingID={personioTrackingID}
+    <>
+      <div id="jobsList" className={styles.empty}></div>
+      <Section size={'md'} bg={'light'} color={'primary'}>
+        <Container>
+          <Grid
+            justifyContent={'space-between'}
+            alignItems={'start'}
+            fullWidth={true}
+          >
+            <GridColumn sm={12} md={7} lg={7}>
+              <SectionHeading
+                title={title}
+                subtitle={subtitle}
+                description={description}
+                headingType={'h2'}
+                align={'left'}
               />
-            ))}
-          </GridColumn>
-        </Grid>
-      </Container>
-    </Section>
+            </GridColumn>
+            <GridColumn sm={12} md={12} lg={12}>
+              {sortedJobs.map((item) => (
+                <JobItem
+                  key={item._id}
+                  job={item}
+                  personioTrackingID={personioTrackingID}
+                />
+              ))}
+            </GridColumn>
+          </Grid>
+        </Container>
+      </Section>
+    </>
   );
 };
