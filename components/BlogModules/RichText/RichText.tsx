34d72ca794f87d0159a2a2ba121259b50013e28f
MichaelaSB-ON <michaela@madebyon.com>
2023-05-15 16:21:20 +0100

    add external link to richtext

--- components/BlogModules/RichText/RichText.tsx
+++ components/BlogModules/RichText/RichText.tsx
@@ -26,6 +26,16 @@ export const RichText: React.FC<any> = ({ content }) => {
               </a>
             );
           },
+          link: ({ value, children }) => {
+            const { blank, href } = value;
+            return blank ? (
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                {children}
+              </a>
+            ) : (
+              <a href={href}>{children}</a>
+            );
+          },
         },
       }}
     />
