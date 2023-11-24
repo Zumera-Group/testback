756d4247309741fea7909bd9abbbbe70eb3442ce
Joseph <anselljoseph19@gmail.com>
2023-11-24 18:10:04 +0000

    saving progress

---
+++ components/TextBlock/TextBlock.tsx
@@ -0,0 +1,25 @@
+import styles from './TextBlock.module.scss';
+import { RichText } from '../RichText';
+import { Container, Grid, GridColumn } from 'components/Layout';
+
+export const TextBlock: React.FC<any> = ({ specificContentModule }) => {
+  const { subheading, text, anchor } = specificContentModule;
+
+  return (
+    <Container classes={styles.textBlock} id={anchor}>
+      <div className={styles.innerOffset}>
+        <Grid
+          fullWidth={true}
+          justifyContent={'space-between'}
+          className={styles.content}
+        >
+          <GridColumn sm={12} md={12} lg={8}>
+            {subheading && <h4>{subheading}</h4>}
+            <RichText content={text} />
+          </GridColumn>
+        </Grid>
+      </div>
+    </Container>
+  );
+};
+export default TextBlock;
