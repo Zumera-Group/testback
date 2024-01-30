00a57b75269fbf5ef422a9acc83216da3a40dbf3
Christos Paschalidis <paschalidi@outlook.com>
2024-01-29 22:58:38 -0600

    cypress: fixes the FR translations after order being updated

--- cypress/e2e/value-calculator/questionnaire-fr.cy.js
+++ cypress/e2e/value-calculator/questionnaire-fr.cy.js
@@ -10,173 +10,167 @@ describe('fills the French calculator', {
 
 
   it('long calculator funnel and see the calendly', () => {
-    cy.visit(`${baseUrl}/fr/des-questionnaires/%C3%A9valuationdesentreprises/`);
+    cy.visit(`${baseUrl}/fr/des-questionnaires/%C3%A9valuationdesentreprises`);
     cy.get(radioButton, { timeout: 10000 }).should('be.visible');
     // if the cookie banner is visible, click the button to accept all cookies
     if (!baseUrl.includes('localhost')) {
       cy.acceptCookiesIfPresent();
     }
-    cy.contains('1 / 34');
+    cy.contains('1 / 33');
     cy.wait(1000);
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('2 / 34');
+    cy.contains('2 / 33');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('3 / 34');
+    cy.contains('3 / 33');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('4 / 34');
+    cy.contains('4 / 33');
     cy.get(checkboxItem).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('5 / 34');
+    cy.contains('5 / 33');
     cy.get('input[type="number"]').type('2015');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('6 / 34');
+    cy.contains('6 / 33');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('7 / 34');
+    cy.contains('7 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('8 / 34');
+    cy.contains('8 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('9 / 34');
-    cy.get('input[type="text"]').type('80');
+    cy.contains('9 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('10 / 34');
+    cy.contains('10 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('11 / 34');
-    cy.get('input[type="text"]').type('1080');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('12 / 34');
+    cy.contains('11 / 33');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('13 / 34');
-    cy.get(checkboxItem).first().click();
+    cy.contains('12 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('14 / 34');
-    cy.get('input[type="text"]').type('99999999999');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-
-    cy.contains('15 / 34');
+    cy.contains('13 / 33');
     cy.get('input[type="text"]').type('9999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('16 / 34');
-    cy.get('input[type="text"]').type('500000');
+    cy.contains('14 / 33');
+    cy.get('input[type="text"]').type('999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('17 / 34');
-    cy.get('input[type="text"]').type('99999999999');
+
+    cy.contains('15 / 33');
+    cy.get('input[type="text"]').type('9999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('18 / 34');
+    cy.contains('16 / 33');
+    cy.get('input[type="text"]').type('99999');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
+    cy.contains('17 / 33');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('19 / 34');
+    cy.contains('18 / 33');
     cy.get('input[type="text"]').type('50');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('20 / 34');
+    cy.contains('19 / 33');
     cy.get('input[type="text"]').type('99999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('21 / 34');
+    cy.contains('20 / 33');
     cy.get('input[type="text"]').type('9999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('22 / 34');
+    cy.contains('21 / 33');
     cy.get('input[type="text"]').type('99999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('23 / 34');
+    cy.contains('22 / 33');
     cy.get('input[type="text"]').type('99999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('24 / 34');
+    cy.contains('23 / 33');
     cy.get('input[type="text"]').clear().type('30');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('25 / 34');
+    cy.contains('24 / 33');
     cy.get('input[type="text"]').type('10000');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('26 / 34');
+    cy.contains('25 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('27 / 34');
+    cy.contains('26 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('28 / 34');
+    cy.contains('27 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('29 / 34');
+    cy.contains('28 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('30 / 34');
+    cy.contains('29 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('31 / 34');
+    cy.contains('30 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('32 / 34');
+    cy.contains('31 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('33 / 34');
+    cy.contains('32 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('34 / 34');
+    cy.contains('33 / 33');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
-    cy.contains('34 / 34');
+
+    cy.contains('33 / 33');
     cy.get('[aria-label="Finish questionnaire button"]').click();
     cy.wait(1000);
 
@@ -191,7 +185,7 @@ describe('fills the French calculator', {
   })
 
   it('short calculator funnel and see the calendly', () => {
-    cy.visit(`${baseUrl}/fr/des-questionnaires/%C3%A9valuationdesentreprisesnoma/`);
+    cy.visit(`${baseUrl}/fr/des-questionnaires/%C3%A9valuationdesentreprisesnoma`);
     cy.get(radioButton, { timeout: 10000 }).should('be.visible');
     // if the cookie banner is visible, click the button to accept all cookies
     if (!baseUrl.includes('localhost')) {
@@ -225,100 +219,97 @@ describe('fills the French calculator', {
     cy.wait(1000);
 
     cy.contains('6 / 26');
-    cy.get(radioButton).first().click();
+    cy.get('input[type="text"]').type('99999999999');
+
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('7 / 26');
+    cy.get('input[type="text"]').type('999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('8 / 26');
+    cy.get('input[type="text"]').type('99999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('9 / 26');
-    cy.get('input[type="text"]').type('30');
+    cy.get('input[type="text"]').type('9999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('10 / 26');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('11 / 26');
-    cy.get('input[type="text"]').type('1080');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('12 / 26');
     cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
+    cy.contains('11 / 26');
+    cy.get('input[type="text"]').clear().type('30');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
+    cy.contains('12 / 26');
+    cy.get('input[type="text"]').type('99999999999');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
     cy.contains('13 / 26');
-    cy.get(checkboxItem).first().click();
+    cy.get('input[type="text"]').type('999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('14 / 26');
-    cy.get('input[type="text"]').type('9999999999');
+    cy.get('input[type="text"]').type('99999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
 
     cy.contains('15 / 26');
-    cy.get('input[type="text"]').type('99999999999');
+    cy.get('input[type="text"]').type('9999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('16 / 26');
-    cy.get('input[type="text"]').type('500000');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('17 / 26');
-    cy.get('input[type="text"]').type('99999999999');
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('18 / 26');
-    cy.get(radioButton).first().click();
-    cy.get('[aria-label="Go to next question button"]').click();
-    cy.wait(1000);
-
-    cy.contains('19 / 26');
     cy.get('input[type="text"]').type('50');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
+    cy.contains('17 / 26');
+    cy.get('input[type="text"]').type('10000');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
+    cy.contains('18 / 26');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
+    cy.contains('19 / 26');
+    cy.get('[aria-label="Go to next question button"]').click();
+    cy.wait(1000);
+
     cy.contains('20 / 26');
-    cy.get('input[type="text"]').type('99999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('21 / 26');
-    cy.get('input[type="text"]').type('9999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('22 / 26');
-    cy.get('input[type="text"]').type('999999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('23 / 26');
-    cy.get('input[type="text"]').type('99999999');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('24 / 26');
-    cy.get('input[type="text"]').clear().type('30');
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
     cy.contains('25 / 26');
-    cy.get('input[type="text"]').type('10000');
+    cy.get(radioButton).first().click();
     cy.get('[aria-label="Go to next question button"]').click();
     cy.wait(1000);
 
@@ -327,7 +318,7 @@ describe('fills the French calculator', {
     cy.wait(1000);
 
 
-    cy.contains('68');
+    cy.contains('60');
     cy.get('input[type="text"]').first().type('Automated Testing suite');
     cy.get('input[type="email"]').first().type('automates-tests@test.com');
     cy.get('input[type="tel"]').first().type('+4917000000000');
